d0cfb12ea5f1c6e8eda8e7b2b30bcc4c5c06f1f3
oneo <670645720@qq.com>
2021-03-25 17:29:20 +0800

    start use vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,39 @@
+const JSNOTE_CHAPTERS = [
+    {
+        text: "简介",
+        link: "/JsNote/introduce" 
+    },
+    {
+        text: "一.什么是JavaScript",
+        link: "/JsNote/1" 
+    },
+    {
+        text: "二.HTML中的JavaScript",
+        link: "/JsNote/2" 
+    },
+    {
+        text: "三.语言基础",
+        link: "/JsNote/3" 
+    }
+]
+
+module.exports = {
+    base: '/',
+    title: 'Oneo\'s Blog',
+    description: 'Oneo Sanchez\'s Tech Blog',
+    lang: 'en-US',
+    themeConfig: {
+        // lastUpdated: 'Last Update',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'GitHub', link: 'https://github.com/oneothebrave/oneothebrave.github.io' },
+        ],
+        sidebar: [
+            { 
+                text: "JS笔记", 
+                children: JSNOTE_CHAPTERS
+            },
+            { text: "ES6 Promised", link: "/devto-promised" }
+        ]
+    }
+}
---
+++ docs/index.md
@@ -0,0 +1,19 @@
+---
+title: 'Home'
+---
+<!-- # 感谢你看我的博客. -->
+
+## 目录
+
+### 《 JavaScript高级程序设计 》笔记系列
+
+[简介](/JsNote/introduce)
+
+[[第1章] 什么是JavaScript](/JsNote/1)
+
+[[第2章] HTML中的JavaScript](/JsNote/2)
+
+[[第3章] 语言基础](/JsNote/3)
+
+### 翻译
+[ES6中的Promised](/devto-promised.md)
